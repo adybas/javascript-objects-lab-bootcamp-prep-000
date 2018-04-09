a8c010b7d145f280043ea57a1071b38b55beedbb
Anna Dybas <a.m.dybas@gmail.com>
2018-04-09 20:09:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ function deleteFromObjectByKey(object, key) {
   return newObject;
 }
 
++function deleteFromObjectByKey(object, key) {
++  var newObj = Object.assign({}, object);
++  delete newObj.key;
++  return newObj;
++}
+
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
