7419d320d4c42fa23dd4ce646ba74d059792ef86
Anna Dybas <a.m.dybas@gmail.com>
2018-04-09 20:10:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,15 +11,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object)
-  delete newObject.key
+  delete newObject[key]
   return newObject;
 }
-
-+function deleteFromObjectByKey(object, key) {
-+  var newObj = Object.assign({}, object);
-+  delete newObj.key;
-+  return newObj;
-+}
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
